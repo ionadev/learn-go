a36d051ed9da3f20774de9fff6e8c81f49f7f0e0
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-03-15 18:11:47 +0530

    For Loop Defer

--- Defer/defer.go
+++ Defer/defer.go
@@ -1,13 +1,29 @@
 package main
 
+/* Whats Defer OwO
+*  When The Function Is Fully Executed, Then Only The defer call's arguments works
+*  F For Ma So Un-Explainatory English
+ */
+
 import "fmt"
 
 func main() {
-	/* Whats Defer OwO
-	*  When The Function Is Fully Executed, Then Only The defer call's arguments works
-	*  F For Ma So Un-Explainatory English
-	 */
+	basic()
+	forefer()
+}
+
+func basic() {
 	defer fmt.Println("The Function Returned")
 
 	fmt.Println("The Function Exectued!")
+}
+
+func forefer() {
+	fmt.Println("Initialised")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("Closed")
 }
