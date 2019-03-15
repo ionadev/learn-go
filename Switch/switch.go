6f2e6dddf3550138837f1c9dcfb08e4af66fb637
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-03-15 17:52:25 +0530

    Advanced Switch case

--- Switch/switch.go
+++ Switch/switch.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
 
 func main() {
+	basic()
+	weekday()
+}
+
+func basic() {
+
 	fmt.Println("GO Works On ")
 	switch os := runtime.GOOS; os {
 	case "darwin":
@@ -14,5 +21,19 @@ func main() {
 		fmt.Println("Linux.")
 	default:
 		fmt.Printf("OS is %s\n", os)
+	}
+}
+func weekday() {
+	fmt.Println("When Is Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today")
+	case today + 1:
+		fmt.Println("Tomorrow")
+	case today + 2:
+		fmt.Println("Day After Tomorrow")
+	default:
+		fmt.Println("Far Away")
 	}
 }
